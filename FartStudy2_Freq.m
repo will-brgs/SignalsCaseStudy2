cf36f4d4ec7a97a4383782d1aa41b7a3985a8575
Will <Will@Will-laptop.dhcp.wustl.edu>
2024-04-18 17:03:10 -0500

    we balls it out

--- FartStudy2_Freq.m
+++ FartStudy2_Freq.m
@@ -10,26 +10,35 @@ bit_rate = 1/(1 * Tp); %Fb, frequency of bits sent out
 bit_period = 1/bit_rate; % Ts, Time between bits sent out
 Ts = bit_period;
 
-sigma = 2;
+sigma = 0.2;
 %% Define Pulse Shapes
-t = -1*Ts:dt:1*Ts; % time vector
+w = -1*Ts:dt:1*Ts; % frequency vector
 
-pulse_square = ones(1,length(t));
-pulse_square(1,1:round(1/4*length(pulse_square))) = 0;
-pulse_square(1,round(3/4*length(pulse_square)):end) = 0;
+pulse_square_freq = ones(1,length(w));
+pulse_square_freq(1,1:round(1/4*length(pulse_square_freq))) = 0;
+pulse_square_freq(1,round(3/4*length(pulse_square_freq)):end) = 0;
 
-pulse_sinc = sinc((2*t)/Ts);
+pulse_sinc_freq = sinc((2*w)/Ts);
+
+
+pulse_square_time = ifftshift(ifft(pulse_square_freq));
+pulse_sinc_time = ifftshift(ifft(pulse_sinc_freq));
 
 figure, hold on
-subplot(2,1,1), stem(t,pulse_square)
-xlabel('Time'),ylabel('Amplitude'),title('Square Pulse Shape')
-subplot(2,1,2), stem(t,pulse_sinc)
-xlabel('Time'),ylabel('Amplitude'),title('Sinc Pulse Shape')
+subplot(2,2,1), stem(w,pulse_square_freq)
+xlabel('Frequency'),ylabel('Amplitude'),title('Square Pulse in Frequency Domain')
+subplot(2,2,2), plot(abs(pulse_square_time), 'r')
+xlabel('Time'),ylabel('Amplitude'),title('Square Pulse in Time Domain')
+
+subplot(2,2,3), stem(w,pulse_sinc_freq)
+xlabel('Frequency'),ylabel('Amplitude'),title('Sinc Pulse in Frequency Domain')
+subplot(2,2,4), plot(abs(pulse_sinc_time), 'r')
+xlabel('Time'),ylabel('Amplitude'),title('Sinc Pulse in Time Domain')
+
 sgtitle('Pulse Shapes Utilized')
 hold off
-%% Use Function on Square Wave Pulse
-%% Use Function on Sinc Pulse
-[r, xn, decoded, SNR] = poopFunc(pulse_square, sigma);
+%% Use Function on Square Pulse
+[~, xn, decoded, SNR] = poopFunc(abs(pulse_square_time), sigma);
 
 figure, hold on
 subplot(2,1,1),stem(xn)
@@ -42,13 +51,13 @@ hold off
 % display performance results
 error = (sum(xn ~= decoded))/(length(decoded)); 
 
-disp('PERFORMANCE FOR SINC PULSE')
+disp('PERFORMANCE FOR SQUARE PULSE')
 disp(['Bitrate: ' ,num2str(bit_rate), ' bits/second'])
 disp(['Standard Deviation: ' , num2str(sigma)])
 disp(['SNR: ' , num2str(SNR)])
 disp(['Error: ' ,num2str(error*100),' percent'])
 %% Use Function on Sinc Pulse
-[r, xn, decoded, SNR] = poopFunc(pulse_sinc, sigma);
+[~, xn, decoded, SNR] = poopFunc(abs(pulse_sinc_time), sigma);
 
 figure, hold on
 subplot(2,1,1),stem(xn)
@@ -62,7 +71,7 @@ hold off
 error = (sum(xn ~= decoded))/(length(decoded)); 
 
 disp(' ');
-disp('PERFORMANCE FOR SQUARE PULSE')
+disp('PERFORMANCE FOR SINC PULSE')
 disp(['Bitrate: ' ,num2str(bit_rate), ' bits/second'])
 disp(['Standard Deviation: ' , num2str(sigma)])
 disp(['SNR: ' , num2str(SNR)])
