4960593a7b0b18ea79a7739b19f6ba20ffc3f9c6
Will <Will@Will-laptop.dhcp.wustl.edu>
2024-04-18 21:00:34 -0500

    banding it up! okfwrkppar

--- FartStudy2_Freq.m
+++ FartStudy2_Freq.m
@@ -10,15 +10,15 @@ bit_rate = 1/(1 * Tp); %Fb, frequency of bits sent out
 bit_period = 1/bit_rate; % Ts, Time between bits sent out
 Ts = bit_period;
 
-sigma = 0.2;
+sigma = 1;
 %% Define Pulse Shapes
 w = -1*Ts:dt:1*Ts; % frequency vector
 
 pulse_square_freq = ones(1,length(w));
-pulse_square_freq(1,1:round(1/4*length(pulse_square_freq))) = 0;
-pulse_square_freq(1,round(3/4*length(pulse_square_freq)):end) = 0;
+pulse_square_freq(1,1:round(1/4*length(pulse_square_freq))) = -1;
+pulse_square_freq(1,round(3/4*length(pulse_square_freq)):end) = -1;
 
-pulse_sinc_freq = sinc((2*w)/Ts);
+pulse_sinc_freq = sinc((20*w)/Ts);
 
 
 pulse_square_time = ifftshift(ifft(pulse_square_freq));
@@ -76,3 +76,26 @@ disp(['Bitrate: ' ,num2str(bit_rate), ' bits/second'])
 disp(['Standard Deviation: ' , num2str(sigma)])
 disp(['SNR: ' , num2str(SNR)])
 disp(['Error: ' ,num2str(error*100),' percent'])
+
+%% Up/down convert with 3 channel system
+freq1 = 20;
+freq2 = 30;
+freq3 = 40;
+
+band1 = pulse_sinc_freq .* cos(1j*freq1*w);
+band2 = pulse_sinc_freq .* cos(1j*freq2*w);
+band3 = pulse_sinc_freq .* cos(1j*freq3*w);
+
+band1 = conv(pulse_sinc_freq, cos(1j*freq1*w));
+band2 = conv(pulse_sinc_freq, cos(1j*freq2*w));
+band3 = conv(pulse_sinc_freq, cos(1j*freq3*w));
+
+figure, hold on
+subplot(3,1,1),plot(abs(band1))
+xlabel('Index'),ylabel('Amplitude'),title('Band 1, 20Hz')
+subplot(3,1,2),plot(abs(band2))
+xlabel('Index'),ylabel('Amplitude'),title('Band 2, 30Hz')
+subplot(3,1,3),plot(abs(band3))
+xlabel('Index'),ylabel('Amplitude'),title('Band 3, 40 Hz')
+sgtitle('Three Upscaled bands')
+hold off
