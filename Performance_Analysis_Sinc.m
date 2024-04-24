b39c89c7ec2df2054568880ccd230a4a40c3b04d
Will <Will@Will-laptop.dhcp.wustl.edu>
2024-04-23 22:30:01 -0500

    the last one

--- Performance_Analysis_Sinc.m
+++ Performance_Analysis_Sinc.m
@@ -1,6 +1,7 @@
 %% Housekeeping
 clear
 close all
+options = optimoptions('lsqcurvefit', 'Display', 'off');
 %% Generate Input Signal and Add Noise Factor, Bitrate = 1/Tp
 Tp = 0.1; % Half pulse width
 sample_period = Tp/40; % dt, pulse and recieve sample period
@@ -41,7 +42,7 @@ xlabel('Frequency'),ylabel('Amplitude'),title('Sinc Pulse in Frequency Domain')
 
 sgtitle('Pulse Shapes Utilized')
 hold off
-%% Run Communication System For Different Sigma Values - Sinc Pulse Shape
+%% Run Communication System For Different Sigma Values, combined analysis - Sinc Pulse Shape
 
 frequencies = [20,30,40];
 simlength = 100;
@@ -73,9 +74,89 @@ figure, hold on
 scatter(SNR,error_avg, 'filled'), xlabel('SNR'),ylabel('Average Error Rate (%)')
 grid on
 plot((xfit),yfit, 'r', 'linewidth', 2)
-title('Simulation of Various Sigma Values on Three Channels')
+title('Simulation of Various Sigma Values With Averaging of All Channelss')
 legend('Simulation Datapoints', 'Regression Line')
 hold off, grid off
+
+%% Run Communication System For Different Sigma Values, channel analysis - Sinc Pulse Shape
+
+frequencies = [20,30,40];
+simlength = 100;
+internal_avg_length = 10;
+sigma = zeros(simlength,1);
+SNR = zeros(simlength,1);
+error_avg_1 = zeros(simlength,1);
+error_avg_2 = zeros(simlength,1);
+error_avg_3 = zeros(simlength,1);
+
+N = 20;
+for i = 1:simlength
+internal_avg_1 = zeros(internal_avg_length,1);
+internal_avg_2 = zeros(internal_avg_length,1);
+internal_avg_3 = zeros(internal_avg_length,1);
+
+for j = 1:internal_avg_length
+sigma(i) = 0.5 + (0.04 * i);
+[SNR(i),error_1,error_2,error_3] = ComSys(pulse_sinc_time,frequencies,sigma(i),N);
+internal_avg_1(j) = error_1;
+internal_avg_2(j) = error_2;
+internal_avg_3(j) = error_3;
+end
+error_avg_1(i) = sum(internal_avg_1) / length(internal_avg_1);
+error_avg_2(i) = sum(internal_avg_2) / length(internal_avg_2);
+error_avg_3(i) = sum(internal_avg_3) / length(internal_avg_3);
+end
+
+ylimit = max(max(max(error_avg_1),max(error_avg_2)),max(error_avg_3))+1;
+
+figure
+%Channel 1
+subplot(1,3,1)
+scatter(SNR,error_avg_1, 'filled'), hold on
+%regression line
+model = @(b, SNR) b(1) * exp(b(2) * SNR);
+initial_guess = [1, 0.1];
+parameters = lsqcurvefit(model, initial_guess, SNR, error_avg_1);
+xfit = linspace(min(SNR), max(SNR), 100);
+yfit = model(parameters, xfit);
+plot((xfit),yfit, 'r', 'linewidth', 2)
+title('Channel 1')
+xlabel('SNR'),ylabel('Average Error Rate (%)')
+ylim([0,ylimit]);
+legend('Simulation Datapoints', 'Regression Line')
+hold off
+%Channel 2
+subplot(1,3,2)
+scatter(SNR,error_avg_2, 'filled'), hold on
+%regression line
+model = @(b, SNR) b(1) * exp(b(2) * SNR);
+initial_guess = [1, 0.1];
+parameters = lsqcurvefit(model, initial_guess, SNR, error_avg_2);
+xfit = linspace(min(SNR), max(SNR), 100);
+yfit = model(parameters, xfit);
+plot((xfit),yfit, 'r', 'linewidth', 2)
+title('Channel 2')
+xlabel('SNR'),ylabel('Average Error Rate (%)')
+ylim([0,ylimit]);
+legend('Simulation Datapoints', 'Regression Line')
+hold off
+%Channel 3
+subplot(1,3,3)
+scatter(SNR,error_avg_3, 'filled'), hold on
+%regression line
+model = @(b, SNR) b(1) * exp(b(2) * SNR);
+initial_guess = [1, 0.1];
+parameters = lsqcurvefit(model, initial_guess, SNR, error_avg_3);
+xfit = linspace(min(SNR), max(SNR), 100);
+yfit = model(parameters, xfit);
+plot((xfit),yfit, 'r', 'linewidth', 2)
+title('Channel 3')
+xlabel('SNR'),ylabel('Average Error Rate (%)')
+ylim([0,ylimit]);
+legend('Simulation Datapoints', 'Regression Line')
+
+sgtitle('Simulation of Various Sigma Values on Three Channels')
+hold off
 
 %% Run Communication System For Different Channel Bandwidth Values - Sinc Pulse Shape
 frequency_ratios = [2,3,4];
@@ -90,7 +171,7 @@ N = 20;
 for i = 1:simlength
 internal_avg = zeros(internal_avg_length,1);
 for j = 1:internal_avg_length
-frequencies = frequency_ratios .* (100 - (0.95 * i));
+frequencies = frequency_ratios .* (0.02* i);
 bandwidth(i) = frequencies(2) - frequencies(1);
 
 [~,error_1,error_2,error_3] = ComSys(pulse_sinc_time,frequencies,sigma,N);
@@ -151,42 +232,3 @@ title('Simulation of Various Half-Pulse Temporal Widths on Three Channels')
 legend('Simulation Datapoints', 'Regression Line', 'location', 'southeast')
 hold off, grid off
 
-
-%% Run Communication System For Different Bit Sent Values - Sinc Pulse Shape
-frequencies = [20,30,40];
-simlength = 100;
-sigma = 0.5;
-internal_avg_length = 10;
-error_avg = zeros(simlength,1);
-Ts = 10;
-
-N_vals = zeros(simlength,1);
-
-for i = 1:simlength
-internal_avg = zeros(internal_avg_length,1);
-%  Generate new pulse shape with new Ts
-
-N = 10 + (i);
-N_vals(i) = N;
-for j = 1:internal_avg_length
-
-[~,error_1,error_2,error_3] = ComSys(pulse_sinc_time,frequencies,sigma,N);
-
-internal_avg(j) = (error_1 + error_2 + error_3)/3;
-end
-% Calculate average error for each simulation in percent
-error_avg(i) = sum(internal_avg) / length(internal_avg);
-end
-
-%generate regression line
-p = polyfit(N_vals, error_avg, 6);
-xfit = linspace(min(N_vals),max(N_vals),simlength);
-yfit = polyval(p, xfit);
-
-figure, hold on
-scatter(N_vals,error_avg, 'filled'), xlabel('Number of Bits Transmitted'),ylabel('Average Error Rate (%)')
-grid on
-plot((xfit),yfit, 'r', 'linewidth', 2)
-title('Simulation of Various Numbers of Bits Transmitted Over Three Channels')
-legend('Simulation Datapoints', 'Regression Line', 'location', 'southeast')
-hold off, grid off
